9a59ac2276831e0aabe06bc87b20e444715d7412
MelkerC <m.cronmark@gmail.com>
2026-05-15 14:54:00 +0200

    gui

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -3,9 +3,13 @@ package se.su.inlupp;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 
@@ -13,54 +17,27 @@ import javafx.scene.control.*;
 public class Gui extends Application {
 
   public void start(Stage stage) {
-      stage.setTitle("Train rail finder"); // min
-
-      Pane root = new Pane();
-      Label nameLabel = new Label("Name:");
-      nameLabel.setPrefWidth(50);
-      TextField nameField = new TextField();
-      nameField.setPrefWidth(100);
-      Button pressMe = new Button("PressMe");
-      Label resultLabel = new Label();
-      resultLabel.setPrefWidth(100);
-
-
-      pressMe.setOnAction((arg) -> {
-          resultLabel.setText("Hello " + nameField.getText());
-      });
+      stage.setTitle("Train rail finder");
 
 
 
-      root.getChildren().addAll(nameLabel, nameField, pressMe, resultLabel);
-
-      Scene scene = new Scene(root, 300, 300);
-
-      nameLabel.relocate(10, 50);
-      nameField.relocate(10, 50);
-      pressMe.relocate(180, 50);
-      resultLabel.relocate(100, 200);
-
-      stage.setScene(scene);
+      stage.setScene(buildMenuScene());
       stage.show();
 
+  }
 
+  public Scene buildMenuScene(){
+      VBox menuBar = new VBox(25);
 
+      Label nameLabel = new Label("Welcome to the train pathfinder!");
+      Button startButton = new Button("Start");
+      Button saveButton = new Button("Save");
+      Button exitButton = new Button("Exit");
 
+      menuBar.setAlignment(Pos.CENTER);
+      menuBar.getChildren().addAll(nameLabel, startButton, saveButton, exitButton);
 
-/*
-    Graph<String> graph = new ListGraph<String>();
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label label =
-        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-    Button button = new Button("Press me");
-    VBox root = new VBox(3, label, button);
-    root.setAlignment(Pos.CENTER);
-    Scene scene = new Scene(root, 640, 900);
-    stage.setScene(scene);
-    stage.show();
-    */
+      return new Scene(menuBar, 300, 300);
   }
 
   public static void main(String[] args) {
