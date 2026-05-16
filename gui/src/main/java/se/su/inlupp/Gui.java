b87e33028abc18108a3d2c8b45ebe408851b0ad8
MelkerC <m.cronmark@gmail.com>
2026-05-16 12:26:18 +0200

    MainmenuButtons

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,13 +1,10 @@
 package se.su.inlupp;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,14 +16,12 @@ public class Gui extends Application {
   public void start(Stage stage) {
       stage.setTitle("Train rail finder");
 
-
-
-      stage.setScene(buildMenuScene());
+      stage.setScene(buildMenuScene(stage));
       stage.show();
 
   }
 
-  public Scene buildMenuScene(){
+  public Scene buildMenuScene(Stage stage) {
       VBox menuBar = new VBox(25);
 
       Label nameLabel = new Label("Welcome to the train pathfinder!");
@@ -34,10 +29,40 @@ public class Gui extends Application {
       Button saveButton = new Button("Save");
       Button exitButton = new Button("Exit");
 
+      startButton.setOnAction((arg) ->{
+          System.out.println("Starting train pathfinder");
+          stage.setScene(buildTrainScene(stage));
+          stage.show();
+      });
+
+      saveButton.setOnAction((arg) ->{
+          System.out.println("Saving...");
+      });
+
+      exitButton.setOnAction((arg) ->{
+          System.out.println("Exiting the program");
+      });
+
       menuBar.setAlignment(Pos.CENTER);
       menuBar.getChildren().addAll(nameLabel, startButton, saveButton, exitButton);
 
       return new Scene(menuBar, 300, 300);
+  }
+
+  public Scene buildTrainScene(Stage stage) {
+      BorderPane borderPane = new BorderPane();
+
+      Button mainMenuButton = new Button("Main Menu");
+
+      mainMenuButton.setOnAction((arg) ->{
+          System.out.println("Back to Main Menu");
+          stage.setScene(buildMenuScene(stage));
+          stage.show();
+      });
+
+      borderPane.setTop(mainMenuButton);
+
+      return new Scene(borderPane, 300, 300);
   }
 
   public static void main(String[] args) {
